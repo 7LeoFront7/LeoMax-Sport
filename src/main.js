7aec1387099c31c5d407ca7175de52d55566a2a5
Maxim <xonskyfilms@gmail.com>
2024-07-27 15:08:31 +0300

    mob 6

--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
+window.onload = function () {
+    hideAddressBar();
+    window.addEventListener("orientationchange", function () {
+        hideAddressBar();
+    }, false);
+}
+
+function hideAddressBar() {
+    setTimeout(function () {
+        document.body.style.height = window.outerHeight + 'px';
+        setTimeout(function () {
+            window.scrollTo(0, 1);
+        }, 1100);
+    }, 1000);
+    return false;
+}
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
